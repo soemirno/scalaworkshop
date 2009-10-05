82a0a777664570cb89d9f65d87aed7c80de35683
soemirno <soemirno@gmail.com>
2009-10-05 18:26:54 +0200

    made subclass of application

--- parsecsv.scala
+++ parsecsv.scala
@@ -1,9 +1,11 @@
 import java.io._
 import scala.io.Source
 
-val src = Source.fromFile(new File("devnology_network.csv"))
-for (line <- src.getLines){
+object parseCsv extends Application {
+  val src = Source.fromFile(new File("devnology_network.csv"))
+  for (line <- src.getLines){
 
-    // println(line)
+      // println(line)
 
+  }
 }
